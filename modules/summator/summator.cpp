27cddf816b70fa1c2bbd52f01bf19c31c82e937a
wiseblank <nikhil@wisehelm.com>
2024-07-18 17:58:37 -0700

    adding summator module

--- modules/summator/summator.cpp
+++ modules/summator/summator.cpp
@@ -3,5 +3,17 @@
 #include "summator.h"
 
 void Summator::add(int p_value) {
-    
+    count += p_value;
+}
+
+void Summator::reset() {
+    count = 0;
+}
+
+int Summator::get_total() const {
+    return count;
+}
+
+void Summator::_bind_methods() {
+    ClassDB::bind_method(D_METHOD("add", "value"), &Summator::add)
 }
