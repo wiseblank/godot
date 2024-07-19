9f6e665fd8270e45729a39dd54a2b75e8be0e662
wiseblank <nikhil@wisehelm.com>
2024-07-19 16:23:02 -0700

    fix summator module

--- modules/summator/SCsub.py
+++
@@ -1,5 +0,0 @@
-# SCsub
-
-Import('env')
-
-env.add_source_files(env.module_sources, "*.cpp")
---
+++ modules/summator/register_types.cpp
@@ -0,0 +1,19 @@
+/* register_types.cpp */
+
+#include "register_types.h"
+
+#include "core/object/class_db.h"
+#include "summator.h"
+
+void initialize_summator_module(ModuleInitializationLevel p_level) {
+    if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE) {
+        return;
+    }
+    ClassDB::register_class<Summator>();
+}
+
+void uninitialize_summator_module(ModuleInitializationLevel p_level) {
+    if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE) {
+        return;
+    }
+}
